13cb39d9832905a05551c119a038fe3329d59abe
unknown-user <unknown-user@192.168.1.39>
2015-05-08 21:19:25 +0200

    Importación inicial del módulo Splash

---
+++ PantallaSplash/CuentaAtras.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Android.OS;
+using Android.App;
+
+namespace PantallaSplash
+{
+	public class CuentaAtras : CountDownTimer
+	{
+		private Activity _Actividad;
+		private Activity _Lanzar;
+
+		public CuentaAtras(long milisegundos, long cuentaAtras, Activity actividad, Activity lanzar)
+			:base(milisegundos, cuentaAtras){
+
+			_Actividad = actividad;
+			_Lanzar = lanzar;
+
+		}
+
+		public override void OnFinish()
+		{
+
+			_Actividad.StartActivity (_Lanzar.GetType());
+			_Actividad.Finish();
+		}
+
+		public override void OnTick(long millisUntilFinished)
+		{
+			
+		}
+	}
+}
+
---
+++ PantallaSplash/Splash.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Views.Animations;
+
+namespace PantallaSplash
+{
+	//Actividad Principal sin historial.
+	[Activity (Label = "Splash", MainLauncher = true, NoHistory = true)]			
+	public class Splash : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Splash);
+
+			MainActivity main = new MainActivity ();
+			CuentaAtras cuentaAtras = new CuentaAtras (5000, 5000, this, main);
+			cuentaAtras.Start ();
+
+			iniciarAnimacion ();
+		}
+
+		public void iniciarAnimacion()
+		{
+			Animation animacion = AnimationUtils.LoadAnimation (this, Resource.Animation.AnimacionAlpha);
+			animacion.Reset ();
+
+			LinearLayout layout = FindViewById<LinearLayout> (Resource.Id.layout);
+			layout.ClearAnimation ();
+			layout.StartAnimation (animacion);
+
+			animacion = AnimationUtils.LoadAnimation (this, Resource.Animation.AnimacionTranslate);
+			animacion.Reset ();
+
+			ImageView img = FindViewById<ImageView> (Resource.Id.imageView);
+			img.ClearAnimation ();
+			img.StartAnimation (animacion);
+		
+		}
+	}
+}
+
